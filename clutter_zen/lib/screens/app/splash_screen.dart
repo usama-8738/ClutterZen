50fd0e12447b9bc2de0333d325b62614d72d9425
Muhammad Suleman Afzal <sulemanafzal@outlook.com>
2025-10-16 14:52:54 +0500

    chore(app): remove unused import from splash

--- clutter_zen/lib/screens/app/splash_screen.dart
+++ clutter_zen/lib/screens/app/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
