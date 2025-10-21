038c9b9f3929fb2fbca6c03e8e45d3da5527ff6d
Muhammad Suleman Afzal <sulemanafzal@outlook.com>
2025-10-21 22:15:08 +0500

    Wrap storage upload parameters for readability

--- clutter_zen/lib/backend/firebase/firebase_storage_repository.dart
+++ clutter_zen/lib/backend/firebase/firebase_storage_repository.dart
@@ -8,12 +8,13 @@ class FirebaseStorageRepository implements IStorageRepository {
   final FirebaseStorage _storage;
 
   @override
-  Future<String> uploadBytes({required String path, required Uint8List data, String? contentType}) async {
+  Future<String> uploadBytes(
+      {required String path,
+      required Uint8List data,
+      String? contentType}) async {
     final ref = _storage.ref(path);
     final meta = SettableMetadata(contentType: contentType);
     await ref.putData(data, meta);
     return ref.getDownloadURL();
   }
 }
-
-
