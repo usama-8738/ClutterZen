b7079ec4f6c5cb7daa3e37bdc0051e336c9ae5fd
Muhammad Suleman Afzal <sulemanafzal@outlook.com>
2025-10-21 22:17:27 +0500

    Reformat storage repository interface signature

--- clutter_zen/lib/backend/interfaces/storage_repository.dart
+++ clutter_zen/lib/backend/interfaces/storage_repository.dart
@@ -1,7 +1,6 @@
 import 'dart:typed_data';
 
 abstract class IStorageRepository {
-  Future<String> uploadBytes({required String path, required Uint8List data, String? contentType});
+  Future<String> uploadBytes(
+      {required String path, required Uint8List data, String? contentType});
 }
-
-
