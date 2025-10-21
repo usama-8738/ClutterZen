4ef424e3cb9e765411408b83a9fe60592ff88851
Muhammad Suleman Afzal <sulemanafzal@outlook.com>
2025-10-21 22:12:37 +0500

    Wrap parameters in fake storage repository

--- clutter_zen/lib/backend/fakes/fake_storage_repository.dart
+++ clutter_zen/lib/backend/fakes/fake_storage_repository.dart
@@ -3,10 +3,11 @@ import '../interfaces/storage_repository.dart';
 
 class FakeStorageRepository implements IStorageRepository {
   @override
-  Future<String> uploadBytes({required String path, required Uint8List data, String? contentType}) async {
+  Future<String> uploadBytes(
+      {required String path,
+      required Uint8List data,
+      String? contentType}) async {
     // Return a data URL placeholder
     return 'data:$contentType;base64,${data.length}';
   }
 }
-
-
