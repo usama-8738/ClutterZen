8d9dd2319bb420633f4e38198fe5bae6ef3b2e22
Muhammad Suleman Afzal <sulemanafzal@outlook.com>
2025-10-27 22:33:03 +0500

    Fix organization overlay model import path

--- clutter_zen/lib/widgets/organization_zones_overlay.dart
+++ clutter_zen/lib/widgets/organization_zones_overlay.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../../models/vision_models.dart';
+import '../models/vision_models.dart';
 
 class OrganizationZonesOverlay extends StatelessWidget {
   const OrganizationZonesOverlay({
