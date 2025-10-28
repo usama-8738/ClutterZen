be007b0159172308ac8927dd9a2999d679ba1645
Muhammad Suleman Afzal <sulemanafzal@outlook.com>
2025-10-28 22:29:54 +0500

    Replace navigation bar with custom bottom nav

--- clutter_zen/lib/screens/app/root_nav.dart
+++ clutter_zen/lib/screens/app/root_nav.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../widgets/app_drawer.dart';
+import '../../app_firebase.dart';
 import 'home_screen.dart';
 import 'history_screen.dart';
 import 'settings_screen.dart';
@@ -26,15 +27,127 @@ class _RootNavState extends State<RootNav> {
     return Scaffold(
       drawer: const AppDrawer(),
       body: pages[_index],
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _index,
-        onDestinationSelected: (i) => setState(() => _index = i),
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home_outlined), label: 'Home'),
-          NavigationDestination(
-              icon: Icon(Icons.history_outlined), label: 'History'),
-          NavigationDestination(
-              icon: Icon(Icons.person_outline), label: 'Profile'),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 8,
+              offset: Offset(0, -2),
+            ),
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _NavItem(
+                  icon: Icons.home_outlined,
+                  label: 'Home',
+                  isSelected: _index == 0,
+                  onTap: () => setState(() => _index = 0),
+                ),
+                _NavItem(
+                  icon: Icons.history_outlined,
+                  label: 'History',
+                  isSelected: _index == 1,
+                  onTap: () => setState(() => _index = 1),
+                ),
+                _NavItem(
+                  icon: Icons.person_outline,
+                  label: 'Profile',
+                  isSelected: _index == 2,
+                  onTap: () => setState(() => _index = 2),
+                  showUserImage: true,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NavItem extends StatelessWidget {
+  const _NavItem({
+    required this.icon,
+    required this.label,
+    required this.isSelected,
+    required this.onTap,
+    this.showUserImage = false,
+  });
+
+  final IconData icon;
+  final String label;
+  final bool isSelected;
+  final VoidCallback onTap;
+  final bool showUserImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (showUserImage) ...[
+            Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: isSelected ? Colors.green.shade100 : Colors.grey.shade200,
+                border: Border.all(
+                  color: isSelected ? Colors.green : Colors.grey,
+                  width: 2,
+                ),
+              ),
+              child: CircleAvatar(
+                radius: 14,
+                backgroundColor: Colors.transparent,
+                child: AppFirebase.auth.currentUser?.photoURL != null
+                    ? ClipOval(
+                        child: Image.network(
+                          AppFirebase.auth.currentUser!.photoURL!,
+                          width: 28,
+                          height: 28,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Icon(
+                              Icons.person,
+                              size: 20,
+                              color: isSelected ? Colors.green : Colors.grey,
+                            );
+                          },
+                        ),
+                      )
+                    : Icon(
+                        Icons.person,
+                        size: 20,
+                        color: isSelected ? Colors.green : Colors.grey,
+                      ),
+              ),
+            ),
+          ] else ...[
+            Icon(
+              icon,
+              size: 24,
+              color: isSelected ? Colors.black : Colors.grey,
+            ),
+          ],
+          const SizedBox(height: 4),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+              color: isSelected ? Colors.black : Colors.grey,
+            ),
+          ),
         ],
       ),
     );
