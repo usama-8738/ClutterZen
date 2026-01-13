398cfb2f6969a6bd057cf5a2bc6e35051ac19f93
Muhammad Suleman Afzal <sulemanafzal@outlook.com>
2026-01-13 22:27:41 +0500

    chore: remove fake provider implementations

--- lib/backend/fakes/fake_generate_provider.dart
+++
@@ -1,9 +0,0 @@
-import '../interfaces/generate_provider.dart';
-
-class FakeGenerateProvider implements IGenerateProvider {
-  @override
-  Future<String> generateOrganizedImage({required String imageUrl}) async {
-    // Return the same image URL to simulate no-op generation
-    return imageUrl;
-  }
-}
